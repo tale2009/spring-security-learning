4a2904e2e614d401b955b0c87b932b4bba5b85da
kirra <kirra@kirradeMacBook-Pro.local>
2019-03-24 23:06:03 +0800

    Update oauth2/src/main/java/com/security/oauth2test/config/OAuth2ServerConfig.java
    修改正确的配置

--- oauth2/src/main/java/com/security/oauth2test/config/OAuth2ServerConfig.java
+++ oauth2/src/main/java/com/security/oauth2test/config/OAuth2ServerConfig.java
@@ -1,13 +1,21 @@
 package com.security.oauth2test.config;
 
+import com.security.oauth2test.authenticationprovider.CustomProvider;
+import com.security.oauth2test.userdetailservice.CustomUserDetialService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.ProviderManager;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Configuration
 @EnableAuthorizationServer //提供/oauth/authorize,/oauth/token,/oauth/check_token,/oauth/confirm_access,/oauth/error
@@ -20,12 +28,27 @@ public class OAuth2ServerConfig extends AuthorizationServerConfigurerAdapter {
                 .checkTokenAccess("isAuthenticated()")
                 .allowFormAuthenticationForClients();
     }
-    @Autowired
-    private AuthenticationManager authenticationManager;
+
+    public CustomUserDetialService customUserDetialService(){
+        return new CustomUserDetialService();
+    }
+
+    private AuthenticationProvider customProvider(){
+        CustomProvider customProvider = new CustomProvider();
+        customProvider.setUserDetailsService(customUserDetialService());
+        return customProvider;
+    }
+
+    private AuthenticationManager customAuthenticationManager(){
+        List<AuthenticationProvider> authenticationProviders=new ArrayList<AuthenticationProvider>();
+        authenticationProviders.add(customProvider());
+        return new ProviderManager(authenticationProviders);
+    }
 
     @Override
     public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
-        endpoints.authenticationManager(authenticationManager);
+        endpoints.authenticationManager(customAuthenticationManager());
+        endpoints.allowedTokenEndpointRequestMethods(HttpMethod.GET,HttpMethod.POST);
     }
 
     @Override
